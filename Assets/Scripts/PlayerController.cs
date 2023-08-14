3c7dddbee4e9788cf1f0f93b5def922c7ef46429
Joao Arthur Rodrigues da Silva <0364964@feevale.br>
2023-08-14 16:52:38 -0300

    Added project files and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _body;
+
+    private float _horizontal;
+    private float _vertical;
+
+    [SerializeField] private float runSpeed = 7.0f;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _body.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _body.velocity = new Vector2(_horizontal * runSpeed, _vertical * runSpeed);
+    }
+    // private void On
+}
