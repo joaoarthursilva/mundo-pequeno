cdaf387e77c742d389cc09c7088f085fc82ac1e1
Joao Arthur Rodrigues da Silva <0364964@feevale.br>
2023-08-16 18:07:18 -0300

    Added Spider movement and web release

--- Assets/Scripts/Enemies/Spider.cs
+++ Assets/Scripts/Enemies/Spider.cs
@@ -9,9 +9,12 @@ namespace Enemies
         private bool _canMove;
         private Rigidbody2D _rb;
         private Transform _transform;
+        private SpiderMovementPoint[] _spiderMovementPoints;
+        [SerializeField] private GameObject spiderWeb;
 
         private void Start()
         {
+            _spiderMovementPoints = FindObjectsOfType<SpiderMovementPoint>();
             _canMove = true;
             _rb = GetComponent<Rigidbody2D>();
             _transform = gameObject.transform;
@@ -19,12 +22,32 @@ namespace Enemies
 
         private void FixedUpdate()
         {
+            if (new Vector2(_transform.position.x, _transform.position.y) == _targetPosition)
+            {
+                ChangeTarget();
+                ReleaseSpiderWeb();
+            }
+
             if (_canMove) Move();
         }
 
         private void Move()
         {
             _rb.MovePosition(Vector2.MoveTowards(_transform.position, _targetPosition, .1f));
+        }
+
+        private void ChangeTarget()
+        {
+            do
+            {
+                _targetPosition = _spiderMovementPoints[Random.Range(0, _spiderMovementPoints.Length)].transform
+                    .position;
+            } while (_targetPosition == new Vector2(_transform.position.x, _transform.position.y));
+        }
+
+        private void ReleaseSpiderWeb()
+        {
+            Instantiate(spiderWeb, _transform.position, Quaternion.identity);
         }
     }
 }
