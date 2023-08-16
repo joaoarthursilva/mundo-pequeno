341c81859469d29f909cc48e00712fb3b84ede1d
Joao Arthur Rodrigues da Silva <0364964@feevale.br>
2023-08-16 17:43:24 -0300

    Made basic changes to Spider script

--- Assets/Scripts/Enemies/Spider.cs
+++ Assets/Scripts/Enemies/Spider.cs
@@ -4,12 +4,27 @@ namespace Enemies
 {
     public class Spider : Enemy
     {
+        private Vector2 _targetPosition;
+        private Vector2 _startingAnthillPosition;
+        private bool _canMove;
+        private Rigidbody2D _rb;
+        private Transform _transform;
+
         private void Start()
         {
+            _canMove = true;
+            _rb = GetComponent<Rigidbody2D>();
+            _transform = gameObject.transform;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
+            if (_canMove) Move();
+        }
+
+        private void Move()
+        {
+            _rb.MovePosition(Vector2.MoveTowards(_transform.position, _targetPosition, .1f));
         }
     }
 }
