889fd8108e36e2857cf72902ab8c02659fce4a7f
Joao Arthur Rodrigues da Silva <0364964@feevale.br>
2023-08-17 21:46:21 -0300

    Changed ant movement (WIP)

--- Assets/Scripts/Enemies/Ant.cs
+++ Assets/Scripts/Enemies/Ant.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enemies
 {
     public class Ant : Enemy
     {
-        private Rigidbody2D _rb1;
-        private Rigidbody2D _rb2;
+        private Rigidbody2D _rb;
         private Vector2 _targetAnthillPosition;
         private Vector2 _startingAnthillPosition;
         private Transform _transform;
@@ -13,27 +13,32 @@ namespace Enemies
         private bool _canEnterAnthill;
         [SerializeField] private float timeToRespawn = .5f;
         [SerializeField] private float movementDelay = 1f;
-        [SerializeField] private GameObject antSpriteFront;
-        [SerializeField] private GameObject antSpriteBack;
+        [SerializeField] private ManageAnts manageAnts;
+        [SerializeField] private bool isFront;
 
         private void Start()
         {
-            _rb1 = antSpriteFront.GetComponent<Rigidbody2D>();
-            _rb2 = antSpriteBack.GetComponent<Rigidbody2D>();
+            _rb = GetComponent<Rigidbody2D>();
             _transform = gameObject.transform;
-            Spawn();
-            InvokeRepeating(nameof(Move), 0f, movementDelay);
+            if (isFront)
+            {
+                Spawn();
+                InvokeRepeating(nameof(Move), 0f, movementDelay);
+            }
+            else
+            {
+                Invoke(nameof(Spawn), movementDelay);
+                InvokeRepeating(nameof(Move), movementDelay, movementDelay);
+            }
         }
 
         private void Spawn()
         {
             _canEnterAnthill = false;
-            _startingAnthillPosition = ManageAnthills.GetRandomAnthill().transform.position;
-            do
-            {
-                _targetAnthillPosition = ManageAnthills.GetRandomAnthill().transform.position;
-            } while (_targetAnthillPosition == _startingAnthillPosition);
-
+            _startingAnthillPosition = manageAnts.GetSpawn();
+            Debug.Log(_startingAnthillPosition);
+            _targetAnthillPosition = manageAnts.GetCurrentTarget();
+            Debug.Log(_targetAnthillPosition);
             _transform.position = _startingAnthillPosition;
             _canMove = true;
         }
@@ -76,37 +81,33 @@ namespace Enemies
             {
                 if (_toggleMovementDirection)
                 {
-                    _rb1.MovePosition(new Vector2(position.x + 1, position.y));
-                    _rb2.MovePosition(new Vector2(position.x + 1, position.y));
+                    _rb.MovePosition(new Vector2(position.x + 1, position.y));
                     _toggleMovementDirection = false;
                 }
                 else
                 {
-                    _rb1.MovePosition(new Vector2(position.x, position.y + 1));
-                    _rb2.MovePosition(new Vector2(position.x, position.y + 1));
+                    _rb.MovePosition(new Vector2(position.x, position.y + 1));
                     _toggleMovementDirection = true;
                 }
             }
             else if (_targetAnthillPosition.x > position.x)
             {
-                _rb1.MovePosition(new Vector2(position.x + 1, position.y));
-                _rb2.MovePosition(new Vector2(position.x + 1, position.y));
+                _rb.MovePosition(new Vector2(position.x + 1, position.y));
             }
             else if (_targetAnthillPosition.y > position.y)
             {
-                _rb1.MovePosition(new Vector2(position.x, position.y + 1));
-                _rb2.MovePosition(new Vector2(position.x, position.y + 1));
+                _rb.MovePosition(new Vector2(position.x, position.y + 1));
             }
             else if (_targetAnthillPosition.x < position.x)
             {
-                _rb1.MovePosition(new Vector2(position.x - 1, position.y));
-                _rb2.MovePosition(new Vector2(position.x - 1, position.y));
+                _rb.MovePosition(new Vector2(position.x - 1, position.y));
             }
             else if (_targetAnthillPosition.y < position.y)
             {
-                _rb1.MovePosition(new Vector2(position.x, position.y - 1));
-                _rb2.MovePosition(new Vector2(position.x, position.y - 1));
+                _rb.MovePosition(new Vector2(position.x, position.y - 1));
             }
+
+            // _rb.MovePosition(Vector2.MoveTowards(_transform.position, _targetAnthillPosition, .1f));
         }
     }
 }
---
+++ Assets/Scripts/Enemies/ManageAnts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class ManageAnts : MonoBehaviour
+    {
+        private Vector2 _spawn;
+        private Vector2 _currentTarget;
+
+        private void Start()
+        {
+            GetNewTarget();
+            do
+            {
+                GetNewSpawn();
+            } while (_spawn == _currentTarget);
+        }
+
+        public Vector2 GetCurrentTarget()
+        {
+            return _currentTarget;
+        }
+
+        public Vector2 GetNewTarget()
+        {
+            _currentTarget = ManageAnthills.GetRandomAnthill().transform.position;
+            return _currentTarget;
+        }
+
+        public Vector2 GetSpawn()
+        {
+            return _spawn;
+        }
+
+        public Vector2 GetNewSpawn()
+        {
+            _spawn = ManageAnthills.GetRandomAnthill().transform.position;
+            return _spawn;
+        }
+    }
+}
