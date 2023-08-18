eb759e03a1facbf886e49c0b6d3eafe359b3c918
Joao Arthur Rodrigues da Silva <0364964@feevale.br>
2023-08-18 14:55:38 -0300

    Added Player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -11,9 +12,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float normalRunSpeed = 7.0f;
     private float _currentMoveSpeed;
     private bool _canMove;
+    private SpriteRenderer _playerSpriteRenderer;
+
+    [SerializeField] private Animator animator;
+    private static readonly int HorizontalSpeed = Animator.StringToHash("horizontalSpeed");
+    private static readonly int Vertical = Animator.StringToHash("vertical");
 
     private void Start()
     {
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
         _currentMoveSpeed = normalRunSpeed;
         TurnOnMovement();
         _body = GetComponent<Rigidbody2D>();
@@ -24,6 +31,27 @@ public class PlayerController : MonoBehaviour
     {
         _horizontal = Input.GetAxisRaw("Horizontal");
         _vertical = Input.GetAxisRaw("Vertical");
+
+
+        if (Mathf.Abs(_horizontal) > Mathf.Abs(_vertical))
+        {
+            if (_horizontal > 0)
+            {
+                _playerSpriteRenderer.flipX = true;
+            }
+            else
+            {
+                _playerSpriteRenderer.flipX = false;
+            }
+
+            animator.SetFloat(HorizontalSpeed, Mathf.Abs(_horizontal));
+            animator.SetFloat(Vertical, 0);
+        }
+        else
+        {
+            animator.SetFloat(HorizontalSpeed, 0);
+            animator.SetFloat(Vertical, _vertical);
+        }
     }
 
     private void FixedUpdate()
