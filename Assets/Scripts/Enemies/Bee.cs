3c2db813d186681ff3a95a2ae1156807f34dd5bb
joaoarthursilva1 <joaoartsilva@gmail.com>
2023-08-21 17:29:44 -0300

    Fixed Bee movement

--- Assets/Scripts/Enemies/Bee.cs
+++ Assets/Scripts/Enemies/Bee.cs
@@ -1,82 +1,238 @@
-using Pathfinding;
 using UnityEngine;
 
 namespace Enemies
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Bee : Enemy
     {
-        private Transform _target;
-        public float speed = 600f;
-        public float nextWaypointDistance = .5f;
-        private Path _path;
-        private int _currentWaypoint;
-        private Seeker _seeker;
         private Rigidbody2D _rb;
-        public float repeatRate = .1f;
-        private bool _canMove;
+        private Transform _transform;
+        private PlayerController _playerController;
+        private Vector2 _playerPos;
+        [SerializeField] private float movementDelay;
+        private SpriteRenderer _beeSprite;
+        private Animator _beeAnimator;
+        private bool _toggleMovementDirection;
 
         private void Start()
         {
-            _target = FindObjectOfType<PlayerController>().transform;
-            _canMove = true;
-            _seeker = GetComponent<Seeker>();
+            _beeAnimator = GetComponent<Animator>();
+            _beeSprite = GetComponent<SpriteRenderer>();
             _rb = GetComponent<Rigidbody2D>();
-            InvokeRepeating(nameof(UpdatePath), 0f, repeatRate);
+            _transform = gameObject.transform;
+            _playerController = FindObjectOfType<PlayerController>();
+            InvokeRepeating(nameof(Move), 0f, movementDelay);
+            _toggleMovementDirection = false;
         }
 
-        private void UpdatePath()
+        private Vector2 _position;
+        private float _playerX;
+        private float _playerY;
+        private float _beeX;
+        private float _beeY;
+
+        private void Update()
         {
-            if (_seeker.IsDone())
-                _seeker.StartPath(_rb.position, _target.position, OnPathComplete);
+            _position = _transform.position;
+            _playerX = Mathf.FloorToInt(_playerPos.x) + .5f;
+            _playerY = Mathf.FloorToInt(_playerPos.y) + .5f;
+            _beeX = Mathf.FloorToInt(_position.x) + .5f;
+            _beeY = Mathf.FloorToInt(_position.y) + .5f;
+
+            _playerPos = _playerController.transform.position;
+            ManageAnimDirection();
         }
 
-        private void OnPathComplete(Path p)
-        {
-            if (p.error) return;
+        private AnimDirection _currentAnimDirection;
+        private static readonly int AnimDirectionVariable = Animator.StringToHash("direction");
 
-            _path = p;
-            _currentWaypoint = 0;
-        }
-
-        private void FixedUpdate()
+        private void ManageAnimDirection()
         {
-            if (_canMove) Movement();
-        }
-
-        private Vector2 _direction;
-        private void Movement()
-        {
-            if (_path == null) return;
-            if (_currentWaypoint >= _path.vectorPath.Count)
+            _beeSprite.flipX = false;
+            switch (_currentAnimDirection)
             {
-                return;
+                case AnimDirection.Down:
+                    _beeAnimator.SetInteger(AnimDirectionVariable, 0);
+                    break;
+                case AnimDirection.Up:
+                    _beeAnimator.SetInteger(AnimDirectionVariable, 1);
+                    break;
+                case AnimDirection.Left:
+                    _beeAnimator.SetInteger(AnimDirectionVariable, 3);
+                    break;
+                case AnimDirection.Right:
+                    _beeSprite.flipX = true;
+                    _beeAnimator.SetInteger(AnimDirectionVariable, 3);
+                    break;
+            }
+        }
+
+        private void UpdateAnimDirection(AnimDirection dir)
+        {
+            _currentAnimDirection = dir;
+        }
+
+        private enum AnimDirection
+        {
+            Left,
+            Right,
+            Down,
+            Up
+        }
+
+        private enum Direction
+        {
+            None,
+            Left,
+            Right,
+            Down,
+            Up,
+            DiagUpRight,
+            DiagDownRight,
+            DiagUpLeft,
+            DiagDownLeft,
+        }
+
+        private Direction CheckTargetDirection()
+        {
+            if (_playerX > _beeX && _playerY > _beeY)
+            {
+                return Direction.DiagUpRight;
             }
 
-            _direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
-            var force = _direction * (speed * Time.deltaTime);
-            
-            _rb.AddForce(force);
-
-            var distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWaypoint]);
-
-            if (distance < nextWaypointDistance)
+            if (_playerX < _beeX && _playerY < _beeY)
             {
-                _currentWaypoint++;
+                return Direction.DiagDownLeft;
+            }
+
+            if (_playerX > _beeX && _playerY < _beeY)
+            {
+                return Direction.DiagDownRight;
+            }
+
+            if (_playerX < _beeX && _playerY > _beeY)
+            {
+                return Direction.DiagUpLeft;
+            }
+
+            if (_playerX > _beeX)
+            {
+                return Direction.Right;
+            }
+
+            if (_playerY > _beeY)
+            {
+                return Direction.Up;
+            }
+
+            if (_playerX < _beeX)
+            {
+                return Direction.Left;
+            }
+
+            if (_playerY < _beeY)
+            {
+                return Direction.Down;
+            }
+
+            return Direction.None;
+        }
+
+        private void Move()
+        {
+            var direction = CheckTargetDirection();
+
+            switch (direction)
+            {
+                case Direction.DiagDownLeft:
+                    if (_toggleMovementDirection)
+                    {
+                        MoveLeft();
+                        _toggleMovementDirection = false;
+                    }
+                    else
+                    {
+                        MoveDown();
+                        _toggleMovementDirection = true;
+                    }
+
+                    break;
+                case Direction.DiagDownRight:
+                    if (_toggleMovementDirection)
+                    {
+                        MoveRight();
+                        _toggleMovementDirection = false;
+                    }
+                    else
+                    {
+                        MoveDown();
+                        _toggleMovementDirection = true;
+                    }
+
+                    break;
+                case Direction.DiagUpLeft:
+                    if (_toggleMovementDirection)
+                    {
+                        MoveLeft();
+                        _toggleMovementDirection = false;
+                    }
+                    else
+                    {
+                        MoveUp();
+                        _toggleMovementDirection = true;
+                    }
+
+                    break;
+                case Direction.DiagUpRight:
+                    if (_toggleMovementDirection)
+                    {
+                        MoveRight();
+                        _toggleMovementDirection = false;
+                    }
+                    else
+                    {
+                        MoveUp();
+                        _toggleMovementDirection = true;
+                    }
+
+                    break;
+                case Direction.Right:
+                    MoveRight();
+                    break;
+                case Direction.Left:
+                    MoveLeft();
+                    break;
+                case Direction.Down:
+                    MoveDown();
+                    break;
+                case Direction.Up:
+                    MoveUp();
+                    break;
             }
         }
-        
-        public float GetHorizontalSpeed()
+
+        private void MoveUp()
         {
-            return _direction.x;
-        }
-        public float GetVerticalSpeed()
-        {
-            return _direction.y;
+            _rb.MovePosition(new Vector2(_beeX, _beeY + 1));
+            UpdateAnimDirection(AnimDirection.Up);
         }
 
-        public bool IsMovingRight()
+        private void MoveDown()
         {
-            return _direction.x >= 0;
+            _rb.MovePosition(new Vector2(_beeX, _beeY - 1));
+            UpdateAnimDirection(AnimDirection.Down);
+        }
+
+        private void MoveLeft()
+        {
+            _rb.MovePosition(new Vector2(_beeX - 1, _beeY));
+            UpdateAnimDirection(AnimDirection.Left);
+        }
+
+        private void MoveRight()
+        {
+            _rb.MovePosition(new Vector2(_beeX + 1, _beeY));
+            UpdateAnimDirection(AnimDirection.Right);
         }
     }
 }
